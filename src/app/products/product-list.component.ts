c8c46b555f00fd85bf3e848d0086b340cdfd5f04
Oscar Espirilla <oscar.espirilla@laureate.net>
2017-12-11 21:37:49 -0600

    initial commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct, Product } from './product'; 
+import { ProductService } from './product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls : ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string;
+    errorMessage: string = '';
+    filteredProducts : IProduct[];
+    products: IProduct[];
+
+    constructor(private _productService: ProductService)
+    {
+        this.listFilter = 'cart';
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('OnInit');
+        
+        this._productService.getProducts()
+                .subscribe( products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                },
+                            error => this.errorMessage = <any>error);
+
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+                    product.productName.toLocaleLowerCase().indexOf(filterBy) != -1);
+    }
+
+    onRatingClicked(message: string) : void {
+        console.log(message);
+    }
+}
